71034f723572a9e8a4993a4d98e5800c96a8b8b8
TheGauravsahu <itsgauravsahu7@gmail.com>
2026-06-24 17:54:07 +0530

    fix: minor

---
+++ backend/src/app-router-type.ts
@@ -0,0 +1,3 @@
+import type { AppRouter } from "./routers/_app";
+
+export type { AppRouter };
--- frontend/src/lib/trpc.ts
+++ frontend/src/lib/trpc.ts
@@ -1,4 +1,4 @@
-import { createTRPCReact } from '@trpc/react-query';
-import type { AppRouter } from '../../../backend/src/routers/_app';
+import { createTRPCReact } from "@trpc/react-query";
+import type { AppRouter } from "../../../backend/dist/app-router-type";
 
 export const trpc = createTRPCReact<AppRouter>();
